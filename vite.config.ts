55b9bbd6826148ec37b97f5b23ddbe8d82f6f138
bohdan-ryzhko <m3meizumini@gmail.com>
2025-05-19 18:40:37 +0300

    updated base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/synergy_way",
+  base: "/synergy_way/",
   plugins: [react()],
   preview: {
     port: 8080,
